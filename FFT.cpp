365d5692b5cc41282d696876e0dfcfb0b1693444
Tomasz <tomfilip1@gmail.com>
2020-04-15 14:36:50 +0200

    Now Magntude looks a bit sharper

--- FFT.cpp
+++ FFT.cpp
@@ -26,6 +26,19 @@ try {
     // Write FFT rusult images to files
     FFTresult[0].write( "Magnitude.png" );
     cout<<"Magnitude:"<<endl;
+    Pixels view(FFTresult[0]);
+    cout<<FFTresult[0].columns();
+    PixelPacket *pixels = view.get(0,0,FFTresult[0].columns(),FFTresult[0].rows()); 
+    for(size_t i=0; i<FFTresult[0].columns()*FFTresult[0].rows();i++)
+  {
+    
+        pixels->red=5000*log(pixels->red);//log(pixels->red*20+1.0)/log(20+1.0);
+        pixels->green=5000*log(pixels->green);//log(pixels->green*20+1.0)/log(20+1.0);
+        pixels->blue=5000*log(pixels->blue);//log(pixels->blue*20+1.0)/log(20+1.0);
+        pixels++;
+  }  
+
+    view.sync();
     FFTresult[0].display();
   } 
   catch( Exception &error_ ) 
